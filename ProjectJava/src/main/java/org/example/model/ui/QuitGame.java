0d712dd03cf583aa5330aeac2bca6ebce1f0c38a
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2021-12-02 09:08:08 +0200

    improved quit game class

--- ProjectJava/src/main/java/org/example/model/ui/QuitGame.java
+++ ProjectJava/src/main/java/org/example/model/ui/QuitGame.java
@@ -9,25 +9,26 @@ public class QuitGame {
 	}
 
 	public boolean isQuitPrompt() {
-//		Screen.displayMessage("Quit game yes ('y') or no ('n').");
-//
-//		do {
-//			tryAgain = false;
-//			quit = true;
-//
-//			switch (Screen.promptString()) {
-//				case "y":
-//					quit = false;
-//					break;
-//				case "n":
-//					break;
-//				default:
-//					Screen.displayMessage("Invalid option, Try again");
-//					tryAgain = true;
-//					break;
-//			}
-//		} while (tryAgain);
-		quit = true;
+		Screen.displayMessage("Quit game yes ('y') or no ('n').");
+		quit = false;
+		tryAgain = false;
+
+		do {
+			switch (Screen.promptString()) {
+				case "n":
+					quit = true;
+					tryAgain = false;
+					break;
+				case "y":
+					quit = false;
+					tryAgain = false;
+					break;
+				default:
+					Screen.displayMessage("Invalid option, Try again");
+					tryAgain = true;
+					break;
+			}
+		} while (tryAgain);
 		return quit;
 	}
 }
